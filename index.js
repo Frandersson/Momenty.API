71299a0533e52ee85c2f87488742f75e469d0aef
Fredrik Andersson <fredrik1577@gmail.com>
2021-01-25 21:33:00 +0100

    scrape return rates for 3 past months and put them in a table

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const scraper = require('./src/Scraping/scrape');
+
+//scraper.loadFile();
 scraper.getFundReturnRates();
--- src/Scraping/scrape.js
+++ src/Scraping/scrape.js
@@ -2,20 +2,49 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-//const requestUrl = "https://www.di.se/fonder/historik/?SortBy=Diff3mPrc&SortOrder=desc"
+/*
+const requestUrl = "https://www.di.se/fonder/historik/?SortBy=Diff3mPrc&SortOrder=desc"
 
+async function loadFile() {
+    axios.get(requestUrl).then(function(response) {
+
+        const data = response.data;
+
+        fs.writeFile('./output/funds', data, function(err) {
+            if (err) throw err;
+        })
+    })
+}
+*/
 
 async function getFundReturnRates() {
-    fs.readFile('./output/funds', 'utf-8', (err, data) => {
-        if (err) {
-            throw err;
-        }
-
-        const $ = cheerio.load(data);
-        $('table.fixed-table').addClass('123!!!');
+    fs.readFile('./output/funds', 'utf-8', (error, data) => {
+        if (error) throw error;
         
-        console.log($.html());
+        let fundNames = [];
+        let fundReturnRates = [];
+        let fundMap = {};
+        const $ = cheerio.load(data);
+        
+        $('table.fixed-table tbody').first().find('a').each(function() {
+            fundNames.push($(this).text().trim());
+        })
+
+        $('table.scrolling-table tbody').first().find('tr').each(function() {
+            $(this).find('td').each(function(i, v) {
+                if (i == 3) {
+                    fundReturnRates.push($(this).text().trim())
+                }
+            })
+        })
+
+        fundNames.forEach(function(value, index) {
+            fundMap[value] = parseFloat(fundReturnRates[index].replace(",", "."));
+        })
+
+        console.log(JSON.stringify(fundMap));
     })
 }
 
 exports.getFundReturnRates = getFundReturnRates;
+//exports.loadFile = loadFile;
